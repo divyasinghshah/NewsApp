0ad023a9b70c50b11e2b9012443149242ae37628
divyasinghshah <divyasinghshah@gmail.com>
2022-06-04 21:05:05 +0530

    added source, author and data

--- src/components/News.js
+++ src/components/News.js
@@ -62,7 +62,7 @@ export class News extends Component {
       console.log("Next");
       if(this.state.page+1<= Math.ceil(this.state.totalResults/this.props.pageSize))
       {
-        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9c81dad13a8646e4bd4c34757b61e70b&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=business&category=${this.props.category}&apiKey=9c81dad13a8646e4bd4c34757b61e70b&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
         this.setState({loading:true});
         let data= await fetch(url);
         let parsedData= await data.json();
@@ -92,7 +92,7 @@ export class News extends Component {
           {!this.state.loading && this.state.articles.map((element)=>{
              return <div className="col-md-3" key={element.url}>
                  <Newsitem  title={element.title} description={element.description} 
-                 imgUrl={element.urlToImage} newsUrl={element.url} />
+                 imgUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
             </div>
 
           })}
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -3,16 +3,24 @@ import React, { Component } from 'react'
 export class Newsitem extends Component {
    
   render() {
-      let {title, description,imgUrl,newsUrl}=this.props;
+      let {title, description,imgUrl,newsUrl,author,date,source}=this.props;
     return (
       
         <div className='my-3'>
-
+             
             <div className="card" >
+              
+              
             <img src={imgUrl} className="card-img-top" alt="..."/>
+            
             <div className="card-body">
-                <h5 className="card-title">{title}</h5>
+              
+                <h5 className="card-title">{title} <span  class="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:'80%',zIndex:1}}>
+                  {source}
+                    
+                  </span></h5>
                 <p className="card-text">{description}...</p>
+                <p class="card-text"><small class="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
                 <a href={newsUrl} target="blank" className="btn btn-primary btn-sm">Read More</a>
             </div>
             </div>
