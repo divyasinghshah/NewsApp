ff06879d8a7782539a09e73f87193f29459f71a7
divyasinghshah <divyasinghshah@gmail.com>
2022-06-04 19:05:06 +0530

    Previous and Next Page

--- src/components/News.js
+++ src/components/News.js
@@ -8,17 +8,55 @@ export class News extends Component {
         console.log("Hello i am a constructer from news");
         this.state={
             articles:[],
-            loading:false           
+            loading:false ,
+            page:1,
+            totalResults:0          
 
         }
     }
     async componentDidMount(){
-        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=9c81dad13a8646e4bd4c34757b61e70b";
+        let url="https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=9c81dad13a8646e4bd4c34757b61e70b&pageSize=20";
         let data= await fetch(url);
         let parsedData= await data.json();
         console.log(parsedData);
-        this.setState({articles: parsedData.articles})
+        this.setState({articles: parsedData.articles,totalResults:parsedData.totalResults});
         
+    }
+   handlePreviousClick= async()=>{
+     console.log("Previous");
+
+     let url=`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=9c81dad13a8646e4bd4c34757b61e70b&page=${this.state.page-1}&pageSize=20`;
+      let data= await fetch(url);
+      let parsedData= await data.json();
+      console.log(parsedData);
+      
+      
+      this.setState({
+        articles: parsedData.articles,
+        page: this.state.page-1
+      });
+
+
+    }
+    handleNextClick= async ()=>{
+      console.log("Next");
+      if(this.state.page+1<= Math.ceil(this.state.totalResults/20))
+      {
+        let url=`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=9c81dad13a8646e4bd4c34757b61e70b&page=${this.state.page+1}&pageSize=20`;
+        let data= await fetch(url);
+        let parsedData= await data.json();
+        console.log(parsedData);
+        
+        
+        this.setState({
+          articles: parsedData.articles,
+          page: this.state.page+1
+        });
+      }
+      
+     
+
+
     }
   render() {
     return (
@@ -30,7 +68,7 @@ export class News extends Component {
 
           { this.state.articles.map((element)=>{
              return <div className="col-md-3" key={element.url}>
-                 <Newsitem  title={element.title} description={element.description.slice(0,88)} 
+                 <Newsitem  title={element.title} description={element.description} 
                  imgUrl={element.urlToImage} newsUrl={element.url} />
             </div>
 
@@ -41,7 +79,10 @@ export class News extends Component {
           </div>
           
          
-
+          <div className="container  d-flex justify-content-between">
+            <button disabled={this.state.page<=1}type="button" className="btn btn-dark" onClick={this.handlePreviousClick}> &larr;Previous</button>
+            <button disabled={this.state.page+1> Math.ceil(this.state.totalResults/20)}type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+          </div>
 
 
       </div>
